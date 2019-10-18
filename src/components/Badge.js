0bde1c10eec276d9c677d787b5027ea6c24671d3
Bryan Cipriano <ciprianot.bryan@gmail.com>
2019-10-18 01:07:57 -0500

    4. Estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
+import "./styles/Badge.css";
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
     render() {
         return (
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={confLogo} alt="Logo de la conferencia"/>
                 </div>
-                <div>
-                    <img src={confLogo} alt="Avatar"/>
+                <div className="Badge__section-name">
+                    <img
+                        className="Badge__avatar" 
+                        src={confLogo} 
+                        alt="Avatar"/>
                     <h1>
                         Bryan <br/> Cipriano</h1>
                 </div>
-                <div>
-                    <p>Backend Engineer</p>
-                    <p>@ciprianobryan</p>
+                <div className="Badge__section-info">
+                    <h3>Backend Engineer</h3>
+                    <div>@ciprianobryan</div>
                 </div>
-                <div>
+                <div className="Badge__footer">
                     #platziconf
                 </div>
             </div>
