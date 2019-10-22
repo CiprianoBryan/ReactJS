1ae9113506c7bafb92a23bcdb26bb8997fba0445
Bryan Cipriano <ciprianot.bryan@gmail.com>
2019-10-21 21:28:04 -0500

    8. Manejo de estado

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -9,6 +9,25 @@ import header from '../images/badge-header.svg';
 import './styles/BadgeNew.css';
 
 class BadgeNew extends React.Component {
+    state = {
+        form: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            jobTitle: '',
+            twitter: ''
+        }
+    };
+
+    handleChange = e => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
+
     render() {
         return (
             <div>
@@ -20,14 +39,17 @@ class BadgeNew extends React.Component {
                     <div className="row">
                         <div className="col-6">
                             <Badge 
-                                firstName="Bryan"
-                                lastName="Cipriano"
-                                twitter="ciprianoBryan"
-                                jobTitle="Backend Developer"
+                                firstName={this.state.form.firstName}
+                                lastName={this.state.form.lastName}
+                                twitter={this.state.form.twitter}
+                                jobTitle={this.state.form.jobTitle}
+                                email={this.state.form.email}
                             />
                         </div>
                         <div className="col-6">
-                            <BadgeForm/>
+                            <BadgeForm 
+                                onChange={this.handleChange}
+                                formValues={this.state.form}/>
                         </div>
                     </div>
                 </div>
