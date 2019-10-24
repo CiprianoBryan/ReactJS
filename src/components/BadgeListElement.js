28815419670b85fe83d27ba98b061db296ef90c4
Bryan Cipriano <ciprianot.bryan@gmail.com>
2019-10-24 16:32:28 -0500

    15. Mejorando EIUX de una petición

--- src/components/BadgeListElement.js
+++ src/components/BadgeListElement.js
@@ -3,51 +3,51 @@ import {Link} from 'react-router-dom';
 import './styles/BadgeListElement.css';
 
 class BadgeListItem extends React.Component {
-    render() {
-        return (
-            <div className="BadgeListItem">
-                <img
-                    className="BadgeListItem__avatar" 
-                    src={this.props.badge.avatarUrl}
-                    alt="Avatar"
-                />
-                <div>
-                    <strong>{this.props.badge.firstName} {this.props.badge.lastName}
-                    </strong>
-                    <br/>@{this.props.badge.twitter}
-                    <br/>{this.props.badge.jobTitle}
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="BadgeListItem">
+				<img
+					className="BadgeListItem__avatar" 
+					src={this.props.badge.avatarUrl}
+					alt="Avatar"
+				/>
+				<div>
+					<strong>{this.props.badge.firstName} {this.props.badge.lastName}
+					</strong>
+					<br/>@{this.props.badge.twitter}
+					<br/>{this.props.badge.jobTitle}
+				</div>
+			</div>
+		);
+	}
 }
 
 class BadgeListElements extends React.Component {
-    render() {
-        if (this.props.badges.length == 0) {
-            return (
-                <div>
-                    <h3>No badge were found</h3>
-                    <Link className="btn btn-primary" to="/badge/new">
-                        Create new badge
-                    </Link>
-                </div>
-            );
-        }
-        return (
-            <div className="BadgeListElement">
-                <ul className="list-unstyled">
-                    {this.props.badges.map(badge => {
-                        return (
-                            <li key={badge.id}>
-                                <BadgeListItem badge={badge}/>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
+	render() {
+		if (this.props.badges.length == 0) {
+			return (
+				<div>
+					<h3>No badge were found</h3>
+					<Link className="btn btn-primary" to="/badge/new">
+						Create new badge
+					</Link>
+				</div>
+			);
+		}
+		return (
+			<div className="BadgeListElement">
+				<ul className="list-unstyled">
+					{this.props.badges.map(badge => {
+						return (
+							<li key={badge.id}>
+								<BadgeListItem badge={badge}/>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default BadgeListElements;
--- src/pages/BadgeList.js
+++ src/pages/BadgeList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import BadgeListElement from '../components/BadgeListElement';
+import PageLoading from '../components/PageLoading';
+import PageError from '../components/PageError';
 import api from '../api';
 
 import './styles/BadgeList.css';
@@ -8,62 +10,62 @@ import './styles/BadgeList.css';
 import confLogo from '../images/badge-header.svg';
 
 class BadgeList extends React.Component {
-    state = {
-        loading: true,
-        error: null,
-        data: undefined
-    };
+	state = {
+		loading: true,
+		error: null,
+		data: undefined
+	};
 
-    componentDidMount() {
-        this.fetchData();
-    }
+	componentDidMount() {
+		this.fetchData();
+	}
 
-    fetchData = async () => {
-        this.setState({
-            loading: true,
-            error: null
-        });
-        try {
-            const data = await api.badges.list();
-            this.setState({
-                loading: false,
-                data: data
-            });
-        } catch (error) {
-            this.setState({
-                loading: false,
-                error: error
-            });
-        }
-    }
+	fetchData = async () => {
+		this.setState({
+			loading: true,
+			error: null
+		});
+		try {
+			const data = await api.badges.list();
+			this.setState({
+				loading: false,
+				data: data
+			});
+		} catch (error) {
+			this.setState({
+				loading: false,
+				error: error
+			});
+		}
+	}
 
-    render() {
-        if (this.state.loading) {
-            return 'Loading...';
-        }
-        if (this.state.error) {
-            return `error: ${this.state.error.message}`
-        }
-        return (
-            <React.Fragment>
-                <div className="Badges">
-                    <div className="Badges__hero">
-                        <div className="Badges__container">
-                            <img className="Badges_conf-logo" src={confLogo} alt=""></img>
-                        </div>
-                    </div>
-                </div>
-                <div className="Badges__container">
-                    <div className="Badges__buttons">
-                        <Link to="/badge/new" className="btn btn-primary">
-                            New Badge
-                        </Link>
-                    </div>
-                    <BadgeListElement badges={this.state.data}/>
-                </div>
-            </React.Fragment>
-        );
-    }
+	render() {
+		if (this.state.loading) {
+			return <PageLoading/>;
+		}
+		if (this.state.error) {
+			return <PageError error={this.state.error}/>
+		}
+		return (
+			<React.Fragment>
+				<div className="Badges">
+					<div className="Badges__hero">
+						<div className="Badges__container">
+							<img className="Badges_conf-logo" src={confLogo} alt=""></img>
+						</div>
+					</div>
+				</div>
+				<div className="Badges__container">
+					<div className="Badges__buttons">
+						<Link to="/badge/new" className="btn btn-primary">
+							New Badge
+						</Link>
+					</div>
+					<BadgeListElement badges={this.state.data}/>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
 export default BadgeList;
